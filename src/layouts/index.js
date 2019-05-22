c34430b5b0dc4900faecd0e523f470ce9a4c4a80
julien <julien@faakpaat.com>
2019-05-22 14:58:47 +0200

    comingSoon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,53 +1,11 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
-import '../assets/sass/styles.sass'
 import config from '../../data/config'
-import Transition from "../components/Transition";
 import comingSoon from '../assets/img/studio_altad_coming_soon.jpg'
 
 class TemplateWrapper extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      isActive: false,
-      isMiddleLogo: false
-    }
-    this.toggleNavbar = this.toggleNavbar.bind(this)
-  }
-
-  componentDidMount() {
-    if(this.props.location.pathname == '/'){
-      this.setState({isMiddleLogo: false})
-    }
-    else{
-      this.setState({isMiddleLogo: true})
-    }
-  }
-  componentDidUpdate() {
-    if(this.props.location.pathname == '/'){
-      if (this.state.isMiddleLogo) {
-          this.setState({
-            isMiddleLogo: false
-          });
-      }
-    }
-    else{
-      if (!this.state.isMiddleLogo) {
-          this.setState({
-            isMiddleLogo: true
-          });
-      }
-    }
-  }
-
-  toggleNavbar () {
-    this.setState({isActive: !this.state.isActive})
-  }
-
-  toggleServices () {
-    this.setState({isActiveServices: !this.state.isActiveServices})
   }
 
   render () {
