b62e185035dbad6ff25ece107776c7e57c803a24
julien <julien@faakpaat.com>
2019-05-22 15:02:32 +0200

    comingSoon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,8 @@ module.exports = {
     siteUrl: config.siteUrl,
     rssMetadata: {
       site_url: config.siteUrl + pathPrefix,
-      feed_url: config.siteUrl + pathPrefix + config.siteRss,
       title: config.siteTitle,
       description: config.siteDescription,
-      image_url: `${config.siteUrl + pathPrefix}/icons/icon-512x512.png`,
-      author: config.userName,
-      copyright: config.copyright,
     },
   },
   plugins: [
@@ -131,80 +127,6 @@ module.exports = {
             src: `/icons/icon-512x512.png`,
             sizes: `512x512`,
             type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        setup (ref) {
-          const ret = ref.query.site.siteMetadata.rssMetadata
-          ret.allMarkdownRemark = ref.query.allMarkdownRemark
-          ret.generator = config.siteTitle
-          return ret
-        },
-        query: `
-                {
-                  site {
-                    siteMetadata {
-                      rssMetadata {
-                        site_url
-                        feed_url
-                        title
-                        description
-                        image_url
-                        author
-                        copyright
-                      }
-                    }
-                  }
-                }
-              `,
-        feeds: [
-          {
-            serialize (ctx) {
-              const rssMetadata = ctx.query.site.siteMetadata.rssMetadata
-              return ctx.query.allMarkdownRemark.edges
-                .filter(
-                  edge => edge.node.frontmatter.templateKey === 'article-page'
-                )
-                .map(edge => ({
-                  categories: edge.node.frontmatter.tags,
-                  date: edge.node.frontmatter.date,
-                  title: edge.node.frontmatter.title,
-                  description: edge.node.excerpt,
-                  author: rssMetadata.author,
-                  url: rssMetadata.site_url + edge.node.fields.slug,
-                  guid: rssMetadata.site_url + edge.node.fields.slug,
-                  custom_elements: [{'content:encoded': edge.node.html}],
-                }))
-            },
-            query: `
-                    {
-                      allMarkdownRemark(
-                        limit: 1000,
-                        sort: { order: DESC, fields: [frontmatter___date] },
-                      ) {
-                        edges {
-                          node {
-                            html
-                            id
-                            fields { slug }
-                            frontmatter {
-                              title
-                              templateKey
-                              cover
-                              date(formatString: "MMMM DD, YYYY")
-                              tags
-                            }
-                          }
-                        }
-                      }
-                    }
-                  `,
-            output: config.siteRss,
           },
         ],
       },
